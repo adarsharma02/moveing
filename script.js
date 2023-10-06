fecbdd9cd09213c630cf4f332818bef86b719f12
adarsharma02 <you@example.com>
2023-10-06 20:44:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function (val) {
+  val.addEventListener("mouseenter", function () {
+    val.childNodes[3].style.opacity = 1;
+  });
+  val.addEventListener("mouseleave", function () {
+    val.childNodes[3].style.opacity = 0;
+  });
+  val.addEventListener("mousemove", function (ada) {
+    val.childNodes[3].style.left = ada.x + "px";
+    val.childNodes[3].style.top = ada.y + "px";
+  });
+});
